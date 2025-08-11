4662ff1513a3b5bf58a6e1ac8d8c0828dee66858
Snowy Renard <snowyrenard@gmail.com>
2025-08-11 11:38:38 +0200

    Added macro swizzle

--- src/vec/mod.rs
+++ src/vec/mod.rs
@@ -5,3 +5,16 @@ mod vec4;
 pub use vec2::*;
 pub use vec3::*;
 pub use vec4::*;
+
+#[macro_export]
+macro_rules! swizzle {
+    ($(($vec: expr, $a: ident, $b: ident, $c: ident, $d: ident) => $ret: ty)?) => {
+        $(<$ret>::new($vec.$a, $vec.$b, $vec.$c, $vec.$d))?
+    };
+    ($(($vec: expr, $a: ident, $b: ident, $c: ident) => $ret: ty)?) => {
+        $(<$ret>::new($vec.$a, $vec.$b, $vec.$c))?
+    };
+    ($(($vec: expr, $a: ident, $b: ident) => $ret: ty)?) => {
+        $(<$ret>::new($vec.$a, $vec.$b))?
+    };
+}
