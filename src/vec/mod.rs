4c6971f261f4ed5581df174a2fc2d87e90be7f62
Snowy Renard <snowyrenard@gmail.com>
2025-08-12 10:18:52 +0200

    Removed all forms of rep from swizzle

--- src/vec/mod.rs
+++ src/vec/mod.rs
@@ -9,13 +9,13 @@ pub use vec4::*;
 /// A macro for swizzling vectors, the valid inputs are x,y,z,w.
 #[macro_export]
 macro_rules! swizzle {
-    ($(($vec: expr, $a: ident, $b: ident, $c: ident, $d: ident) => $ret: ty)?) => {
-        $(<$ret>::new($vec.$a, $vec.$b, $vec.$c, $vec.$d))?
+    ((($vec: expr, $a: ident, $b: ident, $c: ident, $d: ident) => $ret: ty)) => {
+        (<$ret>::new($vec.$a, $vec.$b, $vec.$c, $vec.$d))
     };
-    ($(($vec: expr, $a: ident, $b: ident, $c: ident) => $ret: ty)?) => {
-        $(<$ret>::new($vec.$a, $vec.$b, $vec.$c))?
+    ((($vec: expr, $a: ident, $b: ident, $c: ident) => $ret: ty)) => {
+        (<$ret>::new($vec.$a, $vec.$b, $vec.$c))
     };
-    ($(($vec: expr, $a: ident, $b: ident) => $ret: ty)?) => {
-        $(<$ret>::new($vec.$a, $vec.$b))?
+    ((($vec: expr, $a: ident, $b: ident) => $ret: ty)) => {
+        (<$ret>::new($vec.$a, $vec.$b))
     };
 }
