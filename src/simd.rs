08c63baf8b830624eead78743426c598651b45c1
Snowy Renard <snowyrenard@gmail.com>
2025-11-11 22:03:32 +0100

    fixed error with simd + num-traits

--- src/simd.rs
+++ src/simd.rs
@@ -1,6 +1,7 @@
 use crate::num::prelude::*;
 use std::simd::{LaneCount, Simd, SimdElement, SupportedLaneCount};
 
+#[cfg(not(feature = "num-traits"))]
 impl<T, const N: usize> Zero for Simd<T, N>
 where
     T: SimdElement + Zero,
@@ -8,6 +9,7 @@ where
 {
     const ZERO: Simd<T, N> = Simd::splat(T::ZERO);
 }
+#[cfg(not(feature = "num-traits"))]
 impl<T, const N: usize> One for Simd<T, N>
 where
     T: SimdElement + One,
