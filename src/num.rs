faaddcc6e3f4311abbcfaabb911007039d5fafb4
Snowy Renard <snowyrenard@gmail.com>
2025-12-01 21:09:07 +0100

    rewrote some macro semenatics for clearer understanding

--- src/num.rs
+++ src/num.rs
@@ -25,7 +25,7 @@ pub trait Float: PartialEq + PartialOrd {
     fn fract(self) -> Self;
 }
 
-#[cfg(not(feature = "num-traits"))]
+#[macro_export]
 macro_rules! derive_float {
     ($($type:ty)+) => {
         $(
@@ -63,14 +63,14 @@ pub mod consts {
     }
 
     macro_rules! impl_const {
-        ($trait:ident $const:ident for $($type:ty: $val:expr)+) => {
+        (impl $trait:ident $const:ident for $($type:ty: $val:expr)+) => {
             $(
                 impl $trait for $type {
                     const $const: Self = $val;
                 }
             )+
         };
-        ($trait:ident $const:ident for $($type:ty)+) => {
+        (impl $trait:ident $const:ident for $($type:ty)+) => {
             $(
                 impl $trait for $type {
                     const $const: Self = <$type>::$const;
@@ -80,9 +80,9 @@ pub mod consts {
     }
 
     #[cfg(not(feature = "num-traits"))]
-    impl_const!(Zero ZERO for usize:0 u8:0 u16:0 u32:0 u128:0 isize:0 i8:0 i16:0 i32:0 i64:0 i128:0 f32:0.0 f64:0.0);
+    impl_const!(impl Zero ZERO for usize:0 u8:0 u16:0 u32:0 u128:0 isize:0 i8:0 i16:0 i32:0 i64:0 i128:0 f32:0.0 f64:0.0);
     #[cfg(not(feature = "num-traits"))]
-    impl_const!(One ONE for usize:1 u8:1 u16:1 u32:1 u128:1 isize:1 i8:1 i16:1 i32:1 i64:1 i128:1 f32:1.0 f64:1.0);
+    impl_const!(impl One ONE for usize:1 u8:1 u16:1 u32:1 u128:1 isize:1 i8:1 i16:1 i32:1 i64:1 i128:1 f32:1.0 f64:1.0);
 
-    impl_const!(NegOne NEG_ONE for isize:-1 i8:-1 i16:-1 i32:-1 i64:-1 i128:-1 f32:-1.0 f64:-1.0);
+    impl_const!(impl NegOne NEG_ONE for isize:-1 i8:-1 i16:-1 i32:-1 i64:-1 i128:-1 f32:-1.0 f64:-1.0);
 }
